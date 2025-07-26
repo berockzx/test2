0a8ba84ef2c2d82540cf3467068cbf24fec541f2
berockzx <127618259+berockzx@users.noreply.github.com>
2025-07-26 23:34:02 +0200

    Add files via upload

---
+++ spixi.poker/app/game.js
@@ -0,0 +1,9 @@
+SpixiAppSdk.onInit = function(sessionId, userAddresses) {
+  document.getElementById('game-area').innerHTML = '<p>Connected. Waiting for IXI deposits...</p>';
+  console.log('Session started:', sessionId, userAddresses);
+};
+
+SpixiAppSdk.onNetworkData = function(sender, data) {
+  console.log('Data from', sender, ':', data);
+  // Basic game flow would go here
+};
